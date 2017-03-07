69b22d8e110243d3e602ab3985581cb4c6d569fd
Jhonatan Matheus <hartzJhonatan@gmail.com>
2017-03-07 09:08:45 -0300

    Atualizacao de metodos bubble e quick

--- trabalhoHome/ExercicioOrdenacao.cpp
+++ trabalhoHome/ExercicioOrdenacao.cpp
@@ -7,15 +7,16 @@ void bubble(int* vetor){
 	int aux;
 	int tamanho = 10;
 	do{
-		for(int i = 0; i < tamanho-1; ++i)
+		for(int i = 0; i < tamanho-1; ++i){
 			if(vetor[i] > vetor[i+1]) {
 				aux = vetor[i];
 				vetor[i] = vetor[i+1];
 				vetor[i+1] = aux;
 			}
+		}
 		--tamanho;
 	}while(tamanho > 1);
-}
+};
 
 void quick(int* vetor, int esq=0, int dir=0) {
 	int pivo = esq, i,ch,j;         //Declaração das variavés e inicialização do pivo com o primeiro algarismo da sequencia
@@ -37,19 +38,19 @@ void quick(int* vetor, int esq=0, int dir=0) {
 	if(pivo+1 <= dir){              //verifica se o valor do pivo é menor, indicando que ainda estar dentro das limitações do vetor
 		quick(vetor,pivo+1,dir);      //chama a função para escutar novamente
 	}
-}
+};
 
 int main() {
 	int tempIni01, tempIni02, tempFim01, tempFim02;
 
-	int numeros01[] = {5,4,3,1,2,6,8,7,10,9};
+	int numerosA[] = {5,4,3,1,2,6,8,7,10,9};
 	tempIni01 = clock();
-	bubble(numeros01);
+	bubble(numerosA);//      metodo de ordenacao Bolha
 	tempFim01 = clock();
 
-	int numeros02[] = {5,4,3,1,2,6,8,7,10,9};
+	int numerosB[] = {5,4,3,1,2,6,8,7,10,9};
 	tempIni02 = clock();
-	quick(numeros02);
+	quick(numerosB,0,10);//  metodo de ordenacao Quick
 	tempFim02 = clock();
 
 	cout << "Tempo gasto com o metodo de ordenacao bubble: " << (tempFim01 - tempIni01) << endl;
